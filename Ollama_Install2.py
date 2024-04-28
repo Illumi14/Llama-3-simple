29d49db0191d0889ad86249feb6c6ea8083debd7
Illumi14 <145398159+Illumi14@users.noreply.github.com>
2024-04-28 17:07:20 +0100

    Add files via upload

---
+++ Ollama_Install2.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+os.system("pip install ollama")
+subprocess.call("powershell.exe ollama pull llama3", shell=True) 
+
+with open("OLLAMA_RUN.py", "w") as f:
+    f.write(""" 
+
+import ollama
+
+def again():
+    print("\n")
+    input_question = input("enter a question (or press e to exit): ")
+    if input_question == "e":
+        exit
+    else:
+        stream = ollama.chat(
+            model='llama3',
+            messages=[{'role': 'user', 'content': input_question}],
+            stream=True,
+        )
+
+        for chunk in stream:
+            print(chunk['message']['content'], end='', flush=True)
+        again()
+
+again()
+            
+""")
+
+
+
